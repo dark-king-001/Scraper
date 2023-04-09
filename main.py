302c05d03606cc9166728158fc7cf0c4e0b03b41
dark-king-001 <piyushat115@gmail.com>
2023-04-09 18:26:51 +0530

    bug fixes

--- main.py
+++ main.py
@@ -3,28 +3,28 @@ import time
 import Worker as wk
 import download as dl
 
-class scraper:
-    def __init__(self):
-        print("hello")
+# class scraper:
+#     def __init__(self):
+#         print("hello")
 
-lol = scraper()
-# f = open('permalink/permalink1.json')
-# data = json.load(f)
+# lol = scraper()
+f = open('permalink/permalink.json')
+data = json.load(f)
 
-# operation_length = len(data['items'])
-# print(f'starting operation {operation_length}')
+operation_length = len(data['items'])
+print(f'starting operation {operation_length}')
 
-# for i in range(0,operation_length,1):
-#     if data['items'][i]['no'].split(' ')[0] != '~':
-#         number = data['items'][i]['no'].split(' ')[1]
-#     else:
-#         print("Index out of range")
-#         continue
-#     title = data['items'][i]['title']
-#     link = data['items'][i]['permalink']
-#     print(f'downloading Chapter {number} - {title}')
-#     wk.Worker(f'mkdir downloads/{number}')
-#     time.sleep(0.5)
-#     dl.load(f'/home/piyush/Desktop/Manual_scrape/downloads/{number}',link)
+for i in range(515,operation_length,1):
+    if data['items'][i]['no'].split(' ')[0] != '~':
+        number = data['items'][i]['no'].split(' ')[1]
+    else:
+        print("Index out of range")
+        continue
+    title = data['items'][i]['title']
+    link = data['items'][i]['permalink']
+    print(f'downloading Chapter {number} - {title}')
+    wk.Worker(f'mkdir downloads/{number}')
+    time.sleep(0.2)
+    dl.load(f'/home/piyush/Desktop/Manual_scrape/downloads/{number}',link)
     
-# f.close()
+f.close()

5b313f8731a0f51c0055c106f7e8599e4ef75614
dark-king-001 <piyushat115@gmail.com>
2023-04-10 15:46:32 +0530

    Introduced Link extraction from multiple sources to generate a bundle

---
+++ link_extract.py
@@ -0,0 +1,26 @@
+import json
+
+input_files = [
+    'permalink/files/volume1.json',
+    'permalink/files/volume2.json',
+    'permalink/files/volume3.json',
+    'permalink/files/volume4.json',
+    'permalink/files/volume5.json',
+    'permalink/files/volume6.json',
+    'permalink/files/volume7.json'
+]
+output_file = 'permalink/permalink.json'
+
+links = []
+try:
+    for file in input_files:
+        with open(file) as f:
+            data = json.load(f)
+            for obj in data['items']:
+                links.append(obj['permalink'])
+        print(f'{file} done')
+except Exception as e:
+    # Handle other exceptions here
+    print(f"Error: {e}")
+with open(output_file, "w") as f:
+    json.dump(links, f)
